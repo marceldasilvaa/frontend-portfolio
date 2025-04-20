eaabc4526d8ab4f0d66594819edf8885dff6f72a
Marcel <marcelalvessc@gmail.com>
2025-04-19 23:15:04 -0300

    fix: Atualizando as rotas

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,7 +5,7 @@ const profile = async (data, token) => {
   const config = requestConfig("GET", data, token);
 
   try {
-    const res = await fetch(api + "/users/profile", config)
+    const res = await fetch(api + "users/profile", config)
       .then((res) => res.json())
       .catch((err) => err);
 
@@ -20,7 +20,7 @@ const updateProfile = async (data, token) => {
   const config = requestConfig("PUT", data, token, true);
 
   try {
-    const res = await fetch(api + "/users/", config)
+    const res = await fetch(api + "users/", config)
       .then((res) => res.json())
       .catch((err) => err);
 
@@ -35,7 +35,7 @@ const getUserById = async (id) => {
   const config = requestConfig("GET");
 
   try {
-    const res = await fetch(api + `/users/${id}`, config)
+    const res = await fetch(api + `users/${id}`, config)
       .then((res) => res.json())
       .catch((err) => err);
 
